9158912066eac3244ac0cef1c620c438541b5af3
SanchanaPThomas <sanchanapthomas1011@gmail.com>
2025-09-27 13:16:00 +0530

    Add files via upload

---
+++ binary search.py
@@ -0,0 +1,16 @@
+def binary_search(arr, n, k):
+    left, right = 0, n - 1
+    while left <= right:
+        middle = (left + right) // 2
+        if arr[middle]==k:
+            return middle
+        elif arr[middle] > k:
+            right-=1
+        else:
+            left+=1
+    return -1  # k not found
+
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    arr = list(map(int, input().split()))
+    print(binary_search(arr, n, k))
