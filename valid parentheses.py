5a4ac98d3c2e57f7d194830e423e4de37c8a3ed5
SanchanaPThomas <sanchanapthomas1011@gmail.com>
2026-01-16 22:54:53 +0530

    Add files via upload
    Given a string s containing only '(' , ')' , '{' , '}' , '[' , ']', determine if the input string is valid.
    
    A string is valid if:
    
    Open brackets must be closed by the same type
    
    Open brackets must be closed in the correct order

---
+++ valid parentheses.py
@@ -0,0 +1,13 @@
+def isValid(s):
+    stack = []
+    mp = {')': '(', ']': '[', '}': '{'}
+
+    for ch in s:
+        if ch in mp:  # closing bracket
+            if not stack or stack[-1] != mp[ch]:
+                return False
+            stack.pop()
+        else:  # opening bracket
+            stack.append(ch)
+
+    return len(stack) == 0
